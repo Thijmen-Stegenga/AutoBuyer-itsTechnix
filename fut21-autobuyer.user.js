acab48ea627ab13b9340deb1ee41b266b145b936
Thijmen-Stegenga <54978013+Thijmen-Stegenga@users.noreply.github.com>
2021-02-08 17:11:48 +0100

    Update fut21-autobuyer.user.js

--- fut21-autobuyer.user.js
+++ fut21-autobuyer.user.js
@@ -5,8 +5,7 @@
 // @version      1.4.2
 // @updateURL    https://github.com/oRastor/fut21-web-app/raw/master/fut21-autobuyer.user.js
 // @description  FUT21 Autobuyer
-// @author       Rastor
-// @co-author    Tiebe_V
+// @author       itsTechnix
 // @match        https://www.easports.com/uk/fifa/ultimate-team/web-app/*
 // @match        https://www.ea.com/fifa/ultimate-team/web-app/*
 // @grant        none
